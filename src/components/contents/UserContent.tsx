464e48d579f96690635a21c35e948f5d3b638f58
fiatclone <165413501+FiatClone@users.noreply.github.com>
2025-08-16 23:39:54 +0700

    Update UserContent.tsx

--- src/components/contents/UserContent.tsx
+++ src/components/contents/UserContent.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import ConnectWallet from "../components/ConnectWallet";
+import { CheckoutWidget, getDefaultToken } from "thirdweb/react";
+import { base } from "thirdweb/chains";
 
-export default function UserContent() {
+function App() {
   return (
-    <div>
-      <h2>Profil</h2>
-      <ConnectWallet />
-    </div>
+    <CheckoutWidget
+      client={THIRDWEB_CLIENT}
+      theme="light"
+      chain={base}
+      amount={"2"}
+      tokenAddress="0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+      seller="0xEb0effdFB4dC5b3d5d3aC6ce29F3ED213E95d675"
+      feePayer="seller"
+      name="Concert Ticket"
+      image="https://example.com/concert-ticket.jpg"
+      description="Concert ticket for the upcoming show"
+    />
   );
 }
-
