5411e3eace8b87640f3485200c6b9c69a34a13dd
fiatclone <165413501+FiatClone@users.noreply.github.com>
2025-08-16 17:58:22 +0700

    Update thirdweb.ts

--- src/thirdweb.ts
+++ src/thirdweb.ts
@@ -1,5 +1,5 @@
 
-import { createThirdwebClient } from "thirdweb";
+import { createThirdwebClient } from "@thirdweb-dev/sdk";
 export const client = createThirdwebClient({
   clientId: "91e2d2d514dd899aa83f2f028742a060",
   });
