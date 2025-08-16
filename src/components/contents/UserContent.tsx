78175db9e5926bee417ed43413642a5ddde6a920
fiatclone <165413501+FiatClone@users.noreply.github.com>
2025-08-16 16:00:50 +0700

    Update UserContent.tsx

--- src/components/contents/UserContent.tsx
+++ src/components/contents/UserContent.tsx
@@ -1,1 +1,10 @@
+import React from "react";
+export default function UserContent() {
+  return (
+    <div>
+      <h2>Profil</h2>
+      <p>Koleksi wallet address, NFT, history, dan setting user.</p>
+    </div>
+  );
+}
 
